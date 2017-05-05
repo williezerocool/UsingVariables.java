2b2af049e168048f6eb8d8f8d0096770932a3c83
Willie Suggs <klbooker09@gmail.com>
2017-05-05 01:46:10 -0500

    Add files via upload

---
+++ UsingVariables.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package using.variables;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class UsingVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int roomNumber, graduate;
+        double e;
+        String compSci, name;
+        
+        roomNumber = 113;
+        e = 2.71828;
+        compSci = "computer science";
+        graduate = 2017;
+        name = "Wille";
+        //assignment 11
+        System.out.println("This is room # " + roomNumber);
+        System.out.println("e is close to " + e);
+        System.out.println("I'm learning about " + compSci);
+        //assignment 12
+        System.out.println("My name is " + name + " and I'll graduate in " + graduate);
+        
+    }
+    
+}
